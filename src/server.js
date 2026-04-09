df0e89158ecfa44e5a2fc12808896468dec96180
Alessia de los Ángeles Pérez Palalcios <alessia.perez@epn.edu.ec>
2026-04-08 23:21:33 -0500

    Configuración del server para desplegar

--- src/server.js
+++ src/server.js
@@ -8,9 +8,12 @@ import adminRoutes from './routes/adminRoutes.js';
 const app = express();
 
 // Middlewares
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
-app.use(cors());
-
 
 // Ruta de prueba
 app.get('/', (req, res) => {
