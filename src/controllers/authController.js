eeaf7696ec1dc12475325105185062026b895549
Alessia de los Ángeles Pérez Palalcios <alessia.perez@epn.edu.ec>
2026-04-15 10:08:53 -0500

    Creación de endpoint para obtener el perfil ya sea de administrador, cliente o vendedor

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,11 @@
 import Usuario from '../models/Usuario.js';
-import Vendedor from '../models/Vendedor.js';
-import { comparePassword, hashPassword } from '../helpers/bcrypt.js';
+import { comparePassword } from '../helpers/bcrypt.js';
 import crearTokenJWT from '../helpers/jwt.js';
+import axios from 'axios';
+import generarToken from '../helpers/generarToken.js';
+import Administrador from '../models/Administrador.js';
+import Cliente from '../models/Cliente.js';
+import Vendedor from '../models/Vendedor.js';
 
 // Login del sistema
 const login = async (req, res) => {
@@ -60,7 +64,85 @@ const login = async (req, res) => {
     }
 };
 
+/*
+// Solicitar recuperación de contraseña
+const recuperarPassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+        // Validar que llegue el correo
+        if (!email) {
+            return res.status(400).json({
+                msg: 'Debe ingresar el correo'
+            });
+        }
+        // Buscar usuario en la colección Usuario
+        const usuario = await Usuario.findOne({
+            email: email.toLowerCase().trim()
+        });
+        if (!usuario) {
+            return res.status(404).json({
+                msg: 'Usuario no encontrado'
+            });
+        }
+        // Verificar si el usuario está activo
+        if (!usuario.estado) {
+            return res.status(403).json({
+                msg: 'Usuario inactivo'
+            });
+        }
+        // Generar token y guardarlo en la base
+        const token = generarToken();
+        usuario.token = token;
+        await usuario.save();
+        // Enlace temporal de recuperación
+        const resetLink = `${process.env.FRONTEND_URL}/recuperar-password/${token}`;
+        // Enviar datos a n8n
+        await axios.post(process.env.N8N_WEBHOOK_URL, {
+            email: usuario.email,
+            nombre: usuario.email,
+            resetLink
+        });
+        return res.status(200).json({
+            msg: 'Enlace de recuperación enviado correctamente'
+        });
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Error al recuperar contraseña',
+            error: error.message
+        });
+    }
+};
 
+*/
+// Endpoint para poder obtener el perfil del usuario que esté logueado 
+const obtenerPerfil = async (req, res) => {
+    try {
+        const usuarioId = req.usuario.id;
+        const usuario = await Usuario.findById(usuarioId).select('-password');
+        if (!usuario) {
+            return res.status(404).json({
+                msg: 'Usuario no encontrado'
+            });
+        }
+        let perfil = null;
+        if (usuario.rol === 'ADMINISTRADOR') {
+            perfil = await Administrador.findById(usuario.perfilId);
+        } else if (usuario.rol === 'VENDEDOR') {
+            perfil = await Vendedor.findById(usuario.perfilId);
+        } else if (usuario.rol === 'CLIENTE') {
+            perfil = await Cliente.findById(usuario.perfilId);
+        }
+        return res.status(200).json({
+            msg: 'Perfil obtenido correctamente',
+            usuario, perfil
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Error al obtener el perfil',
+            error: error.message
+        });
+    }
+};
 
-
-export {login};
+export {login, obtenerPerfil};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { login } from '../controllers/authController.js';
+import { login, obtenerPerfil } from '../controllers/authController.js';
+import protegerRuta from '../middleware/authMiddleware.js';
 
 
 
@@ -7,6 +8,10 @@ const router = Router();
 
 // Ruta login
 router.post('/login', login);
-// Ruta para el registro de vendedor
+// Ruta recuperación de contraseña
+//router.post('/recuperar-password', recuperarPassword);
+//Ruta para obtener el perfil del usuario logueado
+router.get('/perfil',protegerRuta ,obtenerPerfil);
 
-export default router;
+
+export default router;  
