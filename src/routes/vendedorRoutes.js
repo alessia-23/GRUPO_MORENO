5ebe828db8332b0beeb934ff3a551be6d8385d2c
Alessia Perez <alessia.perez@epn.edu.ec>
2026-05-06 23:00:30 -0500

    Se añadió la autorización para que el vendedor también pueda listar a los clientes

--- src/routes/vendedorRoutes.js
+++ src/routes/vendedorRoutes.js
@@ -2,10 +2,14 @@ import { Router } from 'express';
 import { registrarClientePorVendedor } from '../controllers/vendedorController.js';
 import protegerRuta from '../middleware/authMiddleware.js';
 import soloVendedor from '../middleware/vendedorMiddleware.js';
+import { listarClientes } from '../controllers/adminController.js';
 
 const router = Router();
 
 // Vendedor registra cliente
 router.post('/registrar-cliente', protegerRuta, soloVendedor, registrarClientePorVendedor);
+
+// Vendedor lista clientes
+router.get('/listar-clientes', protegerRuta, soloVendedor, listarClientes);
 
 export default router;
