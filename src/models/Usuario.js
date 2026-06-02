d7dd7593826a3dc6742157868ead5d716c6cae1d
alessia-23 <alessia.perez@epn.edu.com>
2026-06-01 22:55:10 -0500

    Refactorización del modelo Uusario, para que se puedan cargar fotos en tanto al perfil

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -10,44 +10,49 @@ const usuarioSchema = new mongoose.Schema(
             trim: true,
             lowercase: true
         },
-
         // Contraseña encriptada
         password: {
             type: String,
             required: true,
         },
-
         // Rol del usuario dentro del sistema
         rol: {
             type: String,
             enum: ['ADMINISTRADOR', 'VENDEDOR', 'CLIENTE'],
             required: true
         },
-
         // Estado para activar o inactivar acceso
         estado: {
             type: Boolean,
             default: true
         },
-
         // Token auxiliar para recuperación de contraseña si luego se usa, va a ser temporal y se eliminará después de su uso
         token: {
             type: String,
             default: null
         },
-
         // Id del perfil relacionado
         perfilId: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
             refPath: 'perfilModelo'
         },
-
         // Nombre del modelo relacionado
         perfilModelo: {
             type: String,
             required: true,
             enum: ['Administrador', 'Vendedor', 'Cliente']
+        },
+        // Para la foto de  perfil, se guarda la URL y el public_id de Cloudinary para facilitar su gestión
+        imagen: {
+            url: {
+                type: String,
+                default: null
+            },
+            public_id: {
+                type: String,
+                default: null
+            }
         }
     },
     {
