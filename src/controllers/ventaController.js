7975e4fe8bf2e584d70ad72a17c6acb6bf3502af
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-09 14:41:01 -0500

    Importación del helpers y modelos

--- src/controllers/ventaController.js
+++ src/controllers/ventaController.js
@@ -3,7 +3,9 @@ import Venta from '../models/Venta.js';
 import Producto from '../models/Producto.js';
 import Usuario from '../models/Usuario.js';
 import Pedido from '../models/Pedido.js';
-import { cobrarVentaConTarjeta } from '../helpers/stripeHelper.js';
+import Carrito from '../models/Carrito.js';
+import { calcularTotales } from '../helpers/calcularTotal.js';
+import { cobrarConTarjeta } from '../helpers/stripeHelper.js';
 
 // Crear una venta directa en el local
 const crearVentaDirecta = async (req, res) => {
