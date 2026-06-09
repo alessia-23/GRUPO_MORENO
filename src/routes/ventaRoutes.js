5dab6e91b7bcf7603616440ac4ea5ab095c17231
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-09 15:49:25 -0500

    Creación de ruta para pagar carrito con tarjeta

--- src/routes/ventaRoutes.js
+++ src/routes/ventaRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { crearVentaDirecta, obtenerMisVentas, obtenerDetalleVenta, confirmarTransferenciaVenta, crearVentaDesdePedido, cancelarVenta } from '../controllers/ventaController.js';
+import { crearVentaDirecta, obtenerMisVentas, obtenerDetalleVenta, confirmarTransferenciaVenta, crearVentaDesdePedido, cancelarVenta, pagarCarritoConTarjeta } from '../controllers/ventaController.js';
 import protegerRuta from '../middleware/authMiddleware.js';
 import soloVendedor from '../middleware/vendedorMiddleware.js';
 
@@ -20,7 +20,11 @@ router.put('/confirmar-transferencia/:id', protegerRuta, soloVendedor, confirmar
 // Crear una venta desde un pedido
 router.post('/crear-desde-pedido/:pedidoId', protegerRuta, soloVendedor, crearVentaDesdePedido);
 
+// Pagar carrito con tarjeta
+router.post('/pagar-carrito-tarjeta', protegerRuta, pagarCarritoConTarjeta);
+
 // Cancelar una venta pendiente
 router.put('/cancelar/:id', protegerRuta, soloVendedor, cancelarVenta);
+
 
 export default router;
