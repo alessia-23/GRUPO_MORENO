f21e7f4cb6d336efe5c0975b97e0ebb575601bf8
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-10 03:36:42 -0500

    Ruta para obtener mis quejas y sugerencias

--- src/routes/quejaSugerenciaRoutes.js
+++ src/routes/quejaSugerenciaRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import protegerRuta from '../middleware/authMiddleware.js';
-
-import { crearQuejaSugerencia } from '../controllers/quejaSugerenciaController.js';
+import { crearQuejaSugerencia, obtenerMisQuejasSugerencias } from '../controllers/quejaSugerenciaController.js';
 
 const router = Router();
 
 // Crear queja o sugerencia
 router.post('/crear', protegerRuta, crearQuejaSugerencia);
+
+// Obtener las quejas o sugerencias
+router.get('/mis', protegerRuta, obtenerMisQuejasSugerencias);
 
 export default router;
