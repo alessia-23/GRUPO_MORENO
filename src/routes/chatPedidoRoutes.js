c2e6b5c982ec3d0a0abf829ae58e93c9a3bf14e6
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-10 00:55:37 -0500

    Creación de ruta para marcar chat pedido como leido

--- src/routes/chatPedidoRoutes.js
+++ src/routes/chatPedidoRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import protegerRuta from '../middleware/authMiddleware.js';
-import { obtenerChatPedido, enviarMensajePedido } from '../controllers/chatPedidoController.js';
+import { obtenerChatPedido, enviarMensajePedido, marcarChatPedidoComoLeido } from '../controllers/chatPedidoController.js';
 
 const router = Router();
 
@@ -9,5 +9,8 @@ router.get('/:pedidoId', protegerRuta, obtenerChatPedido);
 
 // Enviar mensaje al chat de un pedido
 router.post('/mensajes/:pedidoId', protegerRuta, enviarMensajePedido);
+
+// Marcar como leido
+router.patch('/leer/:pedidoId', protegerRuta, marcarChatPedidoComoLeido);
 
 export default router;
