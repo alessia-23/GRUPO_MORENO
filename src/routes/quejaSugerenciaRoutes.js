43e5e4ada41542e79372850fd22251e2339db8fa
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-10 12:43:54 -0500

    Creación de ruta para responder una queja o una sugerencia

--- src/routes/quejaSugerenciaRoutes.js
+++ src/routes/quejaSugerenciaRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import protegerRuta from '../middleware/authMiddleware.js';
-import { crearQuejaSugerencia, obtenerMisQuejasSugerencias, obtenerQuejasSugerenciasAdmin } from '../controllers/quejaSugerenciaController.js';
+import { crearQuejaSugerencia, obtenerMisQuejasSugerencias, obtenerQuejasSugerenciasAdmin, responderQuejaSugerencia } from '../controllers/quejaSugerenciaController.js';
 import soloAdmin from '../middleware/adminMiddleware.js';
 
 const router = Router();
@@ -13,5 +13,8 @@ router.get('/mis', protegerRuta, obtenerMisQuejasSugerencias);
 
 // Obtener todas las quejas y sugerencias 
 router.get('/admin', protegerRuta, soloAdmin, obtenerQuejasSugerenciasAdmin);
+
+// Responder una queja o sugerencia
+router.patch('/responder/:id', protegerRuta, soloAdmin, responderQuejaSugerencia);
 
 export default router;
