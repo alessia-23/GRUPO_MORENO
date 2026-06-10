39b10afca54708458d6a01f9d1fc722972127d1f
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-10 12:54:00 -0500

    Creación del socket para quejas y sugerencias

---
+++ src/sockets/quejaSugerenciaSocket.js
@@ -0,0 +1,22 @@
+const quejaSugerenciaSocket = (io) => {
+    io.on('connection', (socket) => {
+        // Sala general donde se conectan los administradores
+        socket.on('unirse-quejas-admin', () => {
+            socket.join('quejas-admin');
+            console.log(`Socket ${socket.id} unido a quejas-admin`);
+        });
+        // Sala personal del usuario cliente/vendedor
+        socket.on('unirse-mis-quejas', (usuarioId) => {
+            socket.join(`mis-quejas-${usuarioId}`);
+            console.log(`Socket ${socket.id} unido a mis-quejas-${usuarioId}`);
+        });
+        socket.on('salir-quejas-admin', () => {
+            socket.leave('quejas-admin');
+        });
+        socket.on('salir-mis-quejas', (usuarioId) => {
+            socket.leave(`mis-quejas-${usuarioId}`);
+        });
+    });
+};
+
+export default quejaSugerenciaSocket;
