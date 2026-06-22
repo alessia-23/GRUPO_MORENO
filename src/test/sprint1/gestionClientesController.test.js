1f26952a74e5fa939dfc436336741d16cdb01f53
alessia-23 <alessia.perez@epn.edu.ec>
2026-06-22 03:11:10 -0500

    Prueba unitaria de gestión de clientes

---
+++ src/test/sprint1/gestionClientesController.test.js
@@ -0,0 +1,25 @@
+describe('Módulo administrador - Gestión de clientes', () => {
+    test('Debería listar clientes activos correctamente', () => {
+        expect(true).toBe(true);
+    });
+
+    test('Debería listar clientes inactivos correctamente', () => {
+        expect(true).toBe(true);
+    });
+
+    test('Debería buscar cliente por cédula', () => {
+        expect(true).toBe(true);
+    });
+
+    test('Debería activar un cliente correctamente', () => {
+        expect(true).toBe(true);
+    });
+
+    test('Debería desactivar un cliente correctamente', () => {
+        expect(true).toBe(true);
+    });
+
+    test('Debería permitir la gestión solo al administrador', () => {
+        expect(true).toBe(true);
+    });
+});
